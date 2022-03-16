1ad52ec05413395e5e1d88dd18aed5f93a44eb7c
Justin Swiger <justin.swiger@me.com>
2022-03-16 17:48:09 -0400

    completed mclip

---
+++ scripts/mclip.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+# mclip.py - a multi-clipboard game
+
+import pyperclip
+import sys
+TEXT = {'agree': """Yes, I agree. That sounds fine to me.""",
+        'busy': """Sorry, can we do this later this week or next week?""",
+        'upsell': """Would you consider making this a monthly donation?"""}
+
+
+if len(sys.argv) < 2:
+    print('Usage: python mclip.py [keyphrase] - copy phrase text')
+
+sys.argv = [input()]
+keyphrase = sys.argv[0]  # the first command line argument is the keyphrase
+
+if keyphrase in TEXT:
+    pyperclip.copy(TEXT[keyphrase])
+    print('Text for ' + keyphrase + ' copied to clipboard.')
+else:
+    print('There is no text for ' + keyphrase)
+
+sys.exit()
